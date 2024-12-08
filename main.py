354b505fd323a0da7067b86bbc898b3c1f4d81a4
max <maxvin13a@gmail.com>
2024-12-08 18:43:42 +0100

    finish

---
+++ main.py
@@ -0,0 +1,86 @@
+import random
+
+
+num = 0
+number = 2
+answer = 0
+answer1 = 0
+answer2 = 'zero'
+answer3 = 'zero'
+answer4 = 'zero'
+answer5 = 'zero'
+
+
+print("want to play a game")
+play = input("yes or not: ")
+if play == "yes":
+    num = random.randint(1, 7)
+    print(random.randint)
+else:
+    print(":(")
+
+if num == 1:
+    answer = int(input("how many people on the planet: "))
+else:
+   num6 = None
+
+if answer == 80000.0000:
+ number = 1
+else:
+    num6 = None
+
+if num == 2:
+    answer1 = int(input("When is the new year: "))
+else:
+ num6 = None
+
+ if answer1 == 1.1:
+     number = 1
+ else:
+    num6 = None
+
+ if num == 3:
+    answer2 = input("The most popular flowers in the world: ")
+ else:
+  num6 = None
+
+if answer2 == 'roza':
+    number = 1
+else:
+ num6 = None
+
+
+if num == 4:
+ answer3 = input("the most popular person in the world: ")
+else:
+    num6 = None
+
+if answer3 == "Elon musk":
+ number = 1
+else:
+ num6 = None
+
+if num == 5:
+ answer4 = input("the best kind of sport: ")
+else:
+   num6 = None
+
+if answer4 == "football":
+ number = 1
+else:
+ num6 = None
+
+if num == 6:
+ answer5 = input("I have a better project: ")
+else:
+    num6 = None
+
+if answer5 == "yes":
+   number = 1
+else:
+   num6 = None
+
+if number == 1:
+  print("You Win!")
+else:
+  print("You lost")
